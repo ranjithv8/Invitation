1a0b0bfe95531ae48478b7b8013088f33fbfc2f7
Ranjith <ranjith.velyetodi@klm.com>
2019-11-26 16:14:15 +0100

    feature: corrected port

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 let express = require("express");
 
+let PORT = process.env.PORT || 5000;
+
 let app = express();
 
 app.use(express.static('public'));
 app.get('/invite',function(req,res){
     res.sendFile(__dirname+'/public/Invitation.html');
 });
-app.listen(80, () => console.log('app listening on port 80!'));
+app.listen(PORT, () => console.log(`app listening on port ${PORT}`));
